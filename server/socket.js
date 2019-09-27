90427d588f6de8cf1ce9f80573bb2305959710dc
Tariq Sanjakdar <T.Sanjakdar@gmail.com>
2019-09-27 23:44:29 +1000

    json file no longer in use - all data operations changed to db

--- server/socket.js
+++ server/socket.js
@@ -253,7 +253,7 @@ module.exports = {
 
             // change user's role
             socket.on('editUser', (userToEdit, updatedRole) => {
-                User.findOneAndUpdate({username: userToEdit}, {role: updatedRole});
+                User.findOneAndUpdate({username: userToEdit}, {role: updatedRole}, () => {});
                 // for(i=0; i<storedData.users.length; i++){
                 //     if(userToEdit == storedData.users[i].username){
                 //         storedData.users[i].role = updatedRole;
@@ -273,8 +273,8 @@ module.exports = {
                     if(err) return console.error(err);
                     deltedUsersId = res._id;
                 });
-                Group.update({}, {$pullAll:{members: deltedUsersId}});
-                Channel.update({}, {$pullAll:{members: deltedUsersId}});
+                await Group.update({members: deltedUsersId}, {$pull: {members: deltedUsersId}}, () => {});
+                await Channel.update({members: deltedUsersId}, {$pull: {members: deltedUsersId}}, () => {});
 
                 // for(i=0; i<storedData.users.length; i++){
                 //     if(userToDelete == storedData.users[i].username){
@@ -301,162 +301,244 @@ module.exports = {
             });
 
             // create new group - current user automatically added to list of invited users
-            socket.on('createGroup', (groupName) => {
-                newGroup = {"name": groupName, "members": [user.username]}
-                storedData.groups.push(newGroup);
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
-                });
+            socket.on('createGroup', async (groupName) => {
+                let newGroup = await new Group({name: groupName, members: [user.id]});
+                newGroup.save();
+
+                // storedData.groups.push(newGroup);
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
             });
 
-            // delete group
-            socket.on('deleteGroup', (groupName) => {
-                for(i=0; i<storedData.groups.length; i++){
-                    if(storedData.groups[i].name == groupName){
-                        storedData.groups.splice(i, 1);
-                    }
-                }
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
+            // delete group and all channels in group
+            socket.on('deleteGroup', async (groupName) => {
+                let deletedGroupsId = '';
+                await Group.findOneAndDelete({name: groupName}, (err, res) => {
+                    if(err) return console.error(err);
+                    deletedGroupsId = res._id;
                 });
+                await Channel.deleteMany({group: deletedGroupsId}, () => {});
+                // for(i=0; i<storedData.groups.length; i++){
+                //     if(storedData.groups[i].name == groupName){
+                //         storedData.groups.splice(i, 1);
+                //     }
+                // }
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
             });
             
             // invite user to group
-            socket.on('inviteToGroup', (groupName, user) => {
-                for(i=0; i<storedData.groups.length; i++){
-                    if(storedData.groups[i].name == groupName){
-                        if(storedData.groups[i].members.includes(user)){
-                            break;
-                        }
-                        else{
-                            storedData.groups[i].members.push(user);
-                        }
-                    }
-                }
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
+            socket.on('inviteToGroup', async (groupName, user) => {
+                let usersId = '';
+                await User.findOne({username: user}, (err, res) => {
+                    usersId = res._id;
                 });
+                await Group.findOneAndUpdate({name: groupName}, {$push: {members: usersId}}, () => {});
+                // for(i=0; i<storedData.groups.length; i++){
+                //     if(storedData.groups[i].name == groupName){
+                //         if(storedData.groups[i].members.includes(user)){
+                //             break;
+                //         }
+                //         else{
+                //             storedData.groups[i].members.push(user);
+                //         }
+                //     }
+                // }
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
 
                 // refresh list of users in group
                 usersInGroup = [];
-                for(i=0; i<storedData.groups.length; i++){
-                    if(storedData.groups[i].name == inGroup){
-                        usersInGroup = storedData.groups[i].members;
+                let groupUserIds = [];
+                let groupId = '';
+                await Group.findOne({name: groupName}, (err, res) => {
+                    if(err) return console.error(err);
+                    groupId = res._id;
+                    groupUserIds = res.members;
+                });
+                await User.find({_id: {$in: groupUserIds}}, (err, res) => {
+                    if(err) return console.error(err);
+                    for(i = 0; i < res.length; i++){
+                        usersInGroup.push(res[i].username);
                     }
-                }
+                });
+                // for(i=0; i<storedData.groups.length; i++){
+                //     if(storedData.groups[i].name == inGroup){
+                //         usersInGroup = storedData.groups[i].members;
+                //     }
+                // }
                 chat.emit('getUsersInGroup', usersInGroup);
             });
             
             // remove user from group
-            socket.on('removeFromGroup', (groupName, user) => {
-                for(i=0; i<storedData.groups.length; i++){
-                    if(storedData.groups[i].name == groupName){
-                        for(x=0; x<storedData.groups[i].members.length; x++){
-                            if(storedData.groups[i].members[x] == user){
-                                storedData.groups[i].members.splice(x, 1);
-                            }
-                        }
-                    }
-                }
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
-                });
+            socket.on('removeFromGroup', async (groupName, user) => {
+                await Group.findOneAndUpdate({name: groupName}, {$pull: {members: user}}, () => {});
+                // for(i=0; i<storedData.groups.length; i++){
+                //     if(storedData.groups[i].name == groupName){
+                //         for(x=0; x<storedData.groups[i].members.length; x++){
+                //             if(storedData.groups[i].members[x] == user){
+                //                 storedData.groups[i].members.splice(x, 1);
+                //             }
+                //         }
+                //     }
+                // }
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
 
                 // refresh list of users in group
                 usersInGroup = [];
-                for(i=0; i<storedData.groups.length; i++){
-                    if(storedData.groups[i].name == inGroup){
-                        usersInGroup = storedData.groups[i].members;
+                let groupUserIds = [];
+                let groupId = '';
+                await Group.findOne({name: groupName}, (err, res) => {
+                    if(err) return console.error(err);
+                    groupId = res._id;
+                    groupUserIds = res.members;
+                });
+                await User.find({_id: {$in: groupUserIds}}, (err, res) => {
+                    if(err) return console.error(err);
+                    for(i = 0; i < res.length; i++){
+                        usersInGroup.push(res[i].username);
                     }
-                }
+                });
+                // for(i=0; i<storedData.groups.length; i++){
+                //     if(storedData.groups[i].name == inGroup){
+                //         usersInGroup = storedData.groups[i].members;
+                //     }
+                // }
                 chat.emit('getUsersInGroup', usersInGroup);
             });
 
             // create new channel - current user automatically added to list of invited users
-            socket.on('createChannel', (channelName, group) => {
-                newChannel = {"name": channelName, "group": group, "members": [user.username]}
-                storedData.channels.push(newChannel);
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
+            socket.on('createChannel', async (channelName, group) => {
+                let groupId = '';
+                await Group.findOne({name: group}, (err, res) => {
+                    if(err) return console.error(err);
+                    groupId = res._id;
                 });
+                let newChannel = await new Channel({name: channelName, group: groupId, members: [user.id]});
+                newChannel.save();
+
+                // storedData.channels.push(newChannel);
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
             });
 
             // delete channel
-            socket.on('deleteChannel', (channelName) => {
-                for(i=0; i<storedData.channels.length; i++){
-                    if(storedData.channels[i].name == channelName){
-                        storedData.channels.splice(i, 1);
-                    }
-                }
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
-                });
+            socket.on('deleteChannel', async (channelName) => {
+                await Channel.findOneAndDelete({name: channelName}, () => {});
+                // for(i=0; i<storedData.channels.length; i++){
+                //     if(storedData.channels[i].name == channelName){
+                //         storedData.channels.splice(i, 1);
+                //     }
+                // }
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
             });
             
             // invite user to channel
-            socket.on('inviteToChannel', (channelName, user) => {
-                for(i=0; i<storedData.channels.length; i++){
-                    if(storedData.channels[i].name == channelName){
-                        if(storedData.channels[i].members.includes(user)){
-                            break;
-                        }
-                        else{
-                            storedData.channels[i].members.push(user);
-                        }
-                    }
-                }
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
+            socket.on('inviteToChannel', async (channelName, user) => {
+                let usersId = '';
+                await User.findOne({username: user}, (err, res) => {
+                    usersId = res._id;
                 });
+                await Channel.findOneAndUpdate({name: channelName}, {$push: {members: usersId}}, () => {});
+                // for(i=0; i<storedData.channels.length; i++){
+                //     if(storedData.channels[i].name == channelName){
+                //         if(storedData.channels[i].members.includes(user)){
+                //             break;
+                //         }
+                //         else{
+                //             storedData.channels[i].members.push(user);
+                //         }
+                //     }
+                // }
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
 
                 // refresh list of users in channel
                 usersInChannel = [];
-                for(i=0; i<storedData.channels.length; i++){
-                    if(storedData.channels[i].name == inChannel){
-                        usersInChannel = storedData.channels[i].members;
+                let channelUserIds = [];
+                let channelId = '';
+
+                await Channel.findOne({name: channelName}, (err, res) => {
+                    if(err) return console.error(err);
+                    channelId = res._id;
+                    channelUserIds = res.members;
+                });
+                await User.find({_id: {$in: channelUserIds}}, (err, res) => {
+                    if(err) return console.error(err);
+                    for(i = 0; i < res.length; i++){
+                        usersInChannel.push(res[i].username);
                     }
-                }
+                });
+                // for(i=0; i<storedData.channels.length; i++){
+                //     if(storedData.channels[i].name == inChannel){
+                //         usersInChannel = storedData.channels[i].members;
+                //     }
+                // }
                 chat.emit('getUsersInChannel', usersInChannel);
             });
             
             // remove user from channel
-            socket.on('removeFromChannel', (channelName, user) => {
-                for(i=0; i<storedData.channels.length; i++){
-                    if(storedData.channels[i].name == channelName){
-                        for(x=0; x<storedData.channels[i].members.length; x++){
-                            if(storedData.channels[i].members[x] == user){
-                                storedData.channels[i].members.splice(x, 1);
-                            }
-                        }
-                    }
-                }
-                fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
-                    if(err){
-                        console.log('fail');
-                    }
-                });
+            socket.on('removeFromChannel', async (channelName, user) => {
+                await Channel.findOneAndUpdate({name: channelName}, {$pull: {members: user}}, () => {});
+                // for(i=0; i<storedData.channels.length; i++){
+                //     if(storedData.channels[i].name == channelName){
+                //         for(x=0; x<storedData.channels[i].members.length; x++){
+                //             if(storedData.channels[i].members[x] == user){
+                //                 storedData.channels[i].members.splice(x, 1);
+                //             }
+                //         }
+                //     }
+                // }
+                // fs.writeFile('./assets/storedData.JSON', JSON.stringify(storedData), function(err){
+                //     if(err){
+                //         console.log('fail');
+                //     }
+                // });
 
                 // refresh list of users in channel
                 usersInChannel = [];
-                for(i=0; i<storedData.channels.length; i++){
-                    if(storedData.channels[i].name == inChannel){
-                        usersInChannel = storedData.channels[i].members;
+                let channelUserIds = [];
+                let channelId = '';
+
+                await Channel.findOne({name: channelName}, (err, res) => {
+                    if(err) return console.error(err);
+                    channelId = res._id;
+                    channelUserIds = res.members;
+                });
+                await User.find({_id: {$in: channelUserIds}}, (err, res) => {
+                    if(err) return console.error(err);
+                    for(i = 0; i < res.length; i++){
+                        usersInChannel.push(res[i].username);
                     }
-                }
+                });
+                // for(i=0; i<storedData.channels.length; i++){
+                //     if(storedData.channels[i].name == inChannel){
+                //         usersInChannel = storedData.channels[i].members;
+                //     }
+                // }
                 chat.emit('getUsersInChannel', usersInChannel);
             });
         });
